c2f0fd269afde4e052cd92e8e3e6e89f057d75ea
Rinesh Chaulagai <backuprinesh@gmail.com>
2024-07-23 09:52:05 +0545

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import wallet from "./pngegg.png";
+import React, { useState } from "react";
 
 function App() {
+  const [selectedValue, setSelectedValue] = useState("INR");
+
+  const handleChange = (event) => {
+    setSelectedValue(event.target.value);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="main">
+      <div className="select-country">
+        <label htmlFor="dropdown"> </label>
+        <select
+          id="dropdown"
+          value={selectedValue}
+          onChange={handleChange}
+          className="select-country-dropdown"
         >
-          Learn React
-        </a>
-      </header>
+          <option value="AUD"> Australia (AUD)</option>
+          <option value="BRL"> Brazil (BRL)</option>
+          <option value="CAD"> Canada (CAD)</option>
+          <option value="CNY"> China (CNY)</option>
+          <option value="EUR"> Eurozone (EUR)</option>
+          <option value="INR"> India (INR)</option>
+          <option value="JPY"> Japan (JPY)</option>
+          <option value="KRW"> South Korea (KRW)</option>
+          <option value="MXN"> Mexico (MXN)</option>
+          <option value="NPR"> Nepal (NPR)</option>
+          <option value="RUB"> Russia (RUB)</option>
+          <option value="SGD"> Singapore (SGD)</option>
+          <option value="ZAR"> South Africa (ZAR)</option>
+          <option value="CHF"> Switzerland (CHF)</option>
+          <option value="USD"> United States (USD)</option>
+          <option value="GBP"> United Kingdom (GBP)</option>
+        </select>
+      </div>
+      <div className="container">
+        <img className="wallet" src={wallet} />
+        <h1>Expense Tracker</h1>
+        <div className="edit_payment">
+          <h2>Your Balance: {selectedValue} XYZ</h2>
+          <button className="add_button">Add</button>
+        </div>
+      </div>
     </div>
   );
 }
